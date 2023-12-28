61aed4d94abc4bcb6b0272bc7940c5d1bc88db5d
Siddharth S Kumar <dev.designer.sid@gmail.com>
2023-12-28 10:31:12 +0530

    daemon: POC

--- cmd/chimes/main.go
+++ cmd/chimes/main.go
@@ -2,12 +2,116 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/devdesignersid/chimes/pkg/reminder"
+	"github.com/sevlyar/go-daemon"
+	"github.com/shirou/gopsutil/process"
 )
 
 func main() {
+	cntxt := &daemon.Context{
+		PidFileName: "chimes.pid",
+		PidFilePerm: 0644,
+		LogFileName: "chimes.log",
+		LogFilePerm: 0640,
+		WorkDir:     "./",
+		Umask:       027,
+		Args:        []string{"[chimes-daemon]"},
+	}
+
+	d, err := cntxt.Reborn()
+	if err != nil {
+		fmt.Println("Unable to run: ", err)
+	}
+
+	if d != nil {
+		return
+	}
+
+	defer cntxt.Release()
+
+	fmt.Println("- - - - - - - - - - - - - - -")
+	fmt.Println("daemon started")
+
+	go worker()
+
+	err = daemon.ServeSignals()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	// Read the PID from the file
+	pidData, err := os.ReadFile("chimes.pid")
+	if err != nil {
+		fmt.Println("Unable to read PID file:", err)
+		return
+	}
+
+	// Parse the PID
+	pid, err := strconv.Atoi(string(pidData))
+	if err != nil {
+		fmt.Println("Unable to parse PID:", err)
+		return
+	}
+
+	// Get the process
+	p, err := process.NewProcess(int32(pid))
+	if err != nil {
+		fmt.Println("Unable to get process:", err)
+		return
+	}
+
+	// Kill the process
+	err = p.Kill()
+	if err != nil {
+		fmt.Println("Unable to kill process:", err)
+		return
+	}
+
+	fmt.Println("Process killed successfully")
+	fmt.Println("daemon terminated")
+
+}
+
+var (
+	stop = make(chan struct{})
+)
+
+func worker() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	logFile, err := os.OpenFile("chimes.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer logFile.Close()
+
+	logger := log.New(logFile, "", log.LstdFlags)
+
+	for {
+		time.Sleep(1 * time.Second)
+
+		select {
+		case killSignal := <-interrupt:
+			fmt.Println("Got signal:", killSignal)
+			stop <- struct{}{}
+			fmt.Println("Worker stopped")
+		default:
+			logger.Println("Checking for due reminders...")
+		}
+	}
+
+}
+
+func getSampleData() {
 	fmt.Println("Chimes")
 
 	inMemoryReminderStorage := reminder.NewInMemoryReminderStorage()
@@ -27,5 +131,4 @@ func main() {
 	for _, reminder := range reminders {
 		fmt.Printf("%s, %s, %s\n", reminder.Message, reminder.Due.Format("January 2, 2006, 3:04 PM"), reminder.Priority)
 	}
-
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/devdesignersid/chimes
 
 go 1.21.5
 
-require github.com/google/uuid v1.5.0 // indirect
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/sevlyar/go-daemon v0.1.6 // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/tklauser/go-sysconf v0.3.13 // indirect
+	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
